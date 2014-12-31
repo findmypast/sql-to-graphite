cca96e5207809742288b1e3c35bd827c4f1c8319
Inisfair <github@faqoff.com>
2014-12-31 15:04:34 -0500

    Python 3.4 Compatible, preserve timestamp from SQL, read DSN from arguments
    Updated to have Python 3.4 compatibility (not tested on 2.x)
    Updated to read S2G_DSN environment variable or accept DSN via arguments
    Added ability to use 3rd column in SQL query to preserve the original
    metric timestamps
    (InfluxDB Limit?) Floats not supported for timestamp, fixed formatting
    with no decimals (losses sub-second precision)

--- sql_to_graphite/__init__.py
+++ sql_to_graphite/__init__.py
@@ -7,28 +7,34 @@ import time
 import sqlalchemy
 
 def get_executor(dsn):
-    engine = sqlalchemy.create_engine(dsn, isolation_level='READ UNCOMMITTED')
+    engine = sqlalchemy.create_engine(dsn)
     connection = engine.connect()
     return connection.execute
 
 def get_info():
     parser = argparse.ArgumentParser(description='Send SQL results to Graphite')
-    parser.add_argument('--graphite-host', metavar='graphite-host', type=str, nargs=1, default=None, help='Host to send metrics to')
-    parser.add_argument('--graphite-port', metavar='graphite-port', type=int, nargs=1, default=2003, help='Graphite port to send metrics to')
-    parser.add_argument('--graphite-prefix', metavar='graphite-prefix', type=str, nargs=1, default=['db'], help='Prefix for metrics')
+    parser.add_argument('--graphite-host', metavar='graphite-host', type=str, default=None, help='Host to send metrics to')
+    parser.add_argument('--graphite-port', metavar='graphite-port', type=int, default=2003, help='Graphite port to send metrics to')
+    parser.add_argument('--graphite-prefix', metavar='graphite-prefix', type=str, default='db', help='Prefix for metrics')
+    parser.add_argument('--dsn', type=str, default=os.environ.get('S2G_DSN'), help='SQLAlchemy DSN for database connection')
+    parser.add_argument('--timestamped-metric', action='store_true', help='Use 3rd column in query containing timestamp values instead of current timestamp')
     return parser.parse_args()
 
-def run(graphite_host, graphite_port, graphite_prefix, queries, executor):
+def run(graphite_host, graphite_port, graphite_prefix, timestamped, queries, executor):
     data = []
     now = time.time()
     sock = _socket_for_host_port(graphite_host, graphite_port)
-    data = map(executor, queries)
+    data = [executor(q) for q in queries]
     for result in data:
         for line in result:
-            metric, value = line[:2]
-            metric = '{}.{} {} {}\n'.format(graphite_prefix, metric, value, now)
-            print metric,
-            sock.sendall(metric)
+            if timestamped:
+                metric, value, timestamp = line[:3]
+                metric = '{}.{} {} {:0.0f}\n'.format(graphite_prefix, metric, value, timestamp.timestamp())
+            else:
+                metric, value = line[:2]
+                metric = '{}.{} {} {:0.0f}\n'.format(graphite_prefix, metric, value, now)
+            print (metric, end=" ")
+            sock.sendall(metric.encode())
     sock.close()
 
 def _socket_for_host_port(host, port):
@@ -40,19 +46,22 @@ def _socket_for_host_port(host, port):
 
 
 def main():
-    dsn = os.environ.get('S2G_DSN')
-    if dsn is None:
-        print 'You must set your DSN in the environment variable `S2G_DSN`'
+    args = get_info()
+    if args.dsn is None:
+        print ('You must set your DSN in the environment variable `S2G_DSN` or the --dsn argument')
         sys.exit(1)
+    else:
+        print ('Using DSN: {}'.format(args.dsn))
 
     queries = sys.stdin.readlines()
-    args = get_info()
+
     run(
-        args.graphite_host[0],
+        args.graphite_host,
         args.graphite_port,
-        args.graphite_prefix[0],
+        args.graphite_prefix,
+        args.timestamped_metric,
         queries,
-        get_executor(dsn),
+        get_executor(args.dsn),
     )
 
 if __name__ == '__main__':
