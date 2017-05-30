403799b8128645c811d5d6c5fe3acd7d4b7cfe81
Gordon McAllister <gmcallister@findmypast.com>
2017-05-30 10:27:12 +0100

    Split queries and minified queries in to daily and historic subfolders.

--- queries/historic/managed_hints_by_day.sql
+++ queries/historic/managed_hints_by_day.sql
@@ -1,12 +1,12 @@
 DECLARE @AnalysisStart DATETIME;
 DECLARE @AnalysisEnd DATETIME;
-SET @AnalysisStart = CONVERT(DATE, DATEADD(DAY, -31, GETDATE()));
+SET @AnalysisStart = CONVERT(DATE, DATEADD(DAY, -367, GETDATE()));
 SET @AnalysisEnd = CONVERT(DATE, DATEADD(DAY, -1, GETDATE()));
 
 -- Number of managed hints per day
-SELECT 'hints.managed.per_day.count', NumManagedHints, DATEDIFF(SECOND,{d '1970-01-01'}, HintManagedOn) FROM 
+SELECT 'hints.managed.per_day.count', NumManagedHints, DATEDIFF(SECOND,{d '1970-01-01'}, HintManagedOn) FROM
 (
-	SELECT CONVERT(DATE, hint.DateUpdated) AS HintManagedOn, COUNT(1) AS NumManagedHints 
+	SELECT CONVERT(DATE, hint.DateUpdated) AS HintManagedOn, COUNT(1) AS NumManagedHints
 	FROM [Data_Tree]..[PersonHint] hint
 	  INNER JOIN [Data_Tree]..[FamilyTree] tree ON tree.Id = hint.FamilyTreeId
 	  INNER JOIN data_tree.dbo.RelationServiceMember rsm ON rsm.Id = tree.RelationServiceMemberId
@@ -16,5 +16,5 @@ SELECT 'hints.managed.per_day.count', NumManagedHints, DATEDIFF(SECOND,{d '1970-
 	  AND hint.DateUpdated != hint.DateCreated -- Avoid accidentally pulling in new hints
 	  AND member.partnership_key = 0 -- US user
 	GROUP BY CONVERT(DATE, hint.DateUpdated)
-) AS ManagedHintsPerDay	
+) AS ManagedHintsPerDay
 ORDER BY CONVERT(DATE, HintManagedOn) ASC
