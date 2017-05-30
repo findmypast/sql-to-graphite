01001311cba680123dcc5f6a94e2fd60583e36dc
Gordon McAllister <gmcallister@findmypast.com>
2017-05-30 10:34:56 +0100

    Hadn't saved changes :(

--- minified_queries/daily/successful_gedcom_tree_uploads_by_day.sql
+++ minified_queries/daily/successful_gedcom_tree_uploads_by_day.sql
@@ -1,18 +1,1 @@
-DECLARE @AnalysisStart DATETIME;
-DECLARE @AnalysisEnd DATETIME;
-SET @AnalysisStart = CONVERT(DATETIME, CONVERT(DATE, DATEADD(DAY, -1, GETDATE())));
-SET @AnalysisEnd = DATEADD(MILLISECOND, -2, CONVERT(DATETIME, CONVERT(DATE, DATEADD(DAY, 1, @AnalysisStart))))
-
-SELECT 'tree.uploads.per_day.count', NumTrees, DATEDIFF(SECOND,{d '1970-01-01'}, TreeUploadDate) FROM
-(
-	SELECT CONVERT(DATE, tree.DateCreated) AS TreeUploadDate, COUNT(1) AS NumTrees
-	FROM [Data_Tree].[dbo].[FamilyTreeTransform] tree_transform
-	  INNER JOIN [Data_Tree].[dbo].[FamilyTree] tree ON tree_transform.FamilyTreeId = tree.Id
-	  INNER JOIN data_tree..RelationServiceMember RSM ON tree.RelationServiceMemberId = rsm.Id
-	  INNER JOIN [Data_FMP]..[all_member] member ON rsm.MemberKey = member.member_key
-	WHERE CONVERT(DATE, tree_transform.DateCreated) BETWEEN @AnalysisStart AND @AnalysisEnd -- Tree was uploaded within the window
-	  AND member.partnership_key = 0 -- US user
-	  AND tree_transform.DataHeader IS NOT NULL -- Upload was successful
-	GROUP BY CONVERT(DATE, tree.DateCreated)
-) AS TreeUploadList
-ORDER BY TreeUploadDate ASC
+DECLARE @AnalysisStart DATETIME; DECLARE @AnalysisEnd DATETIME; SET @AnalysisStart = CONVERT(DATETIME, CONVERT(DATE, DATEADD(DAY, -1, GETDATE()))); SET @AnalysisEnd = DATEADD(MILLISECOND, -2, CONVERT(DATETIME, CONVERT(DATE, DATEADD(DAY, 1, @AnalysisStart)))) SELECT 'tree.uploads.per_day.count', NumTrees, DATEDIFF(SECOND,{d '1970-01-01'}, TreeUploadDate) FROM ( SELECT CONVERT(DATE, tree.DateCreated) AS TreeUploadDate, COUNT(1) AS NumTrees FROM [Data_Tree].[dbo].[FamilyTreeTransform] tree_transform INNER JOIN [Data_Tree].[dbo].[FamilyTree] tree ON tree_transform.FamilyTreeId = tree.Id INNER JOIN data_tree..RelationServiceMember RSM ON tree.RelationServiceMemberId = rsm.Id INNER JOIN [Data_FMP]..[all_member] member ON rsm.MemberKey = member.member_key WHERE CONVERT(DATE, tree_transform.DateCreated) BETWEEN @AnalysisStart AND @AnalysisEnd AND member.partnership_key = 0 AND tree_transform.DataHeader IS NOT NULL GROUP BY CONVERT(DATE, tree.DateCreated) ) AS TreeUploadList ORDER BY TreeUploadDate ASC 
